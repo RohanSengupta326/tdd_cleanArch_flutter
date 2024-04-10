842d4b5b691bc555cb356e3bda23cf480e793a94
RohanSengupta326 <rohansengupta326@gmail.com>
2024-04-10 16:57:46 +0530

    comments

--- test/src/authentication/domain/usecases/create_user_test.dart
+++ test/src/authentication/domain/usecases/create_user_test.dart
@@ -10,6 +10,9 @@ void main() {
   late CreateUser usecase;
   late AuthenticationRepository repository;
 
+  // we create the mock of the dependencies that class requires
+  // and the instantitate that class itself by providing the mocked
+  // dependencies in setUp.
   setUp(() {
     repository = MockAuthRepo();
     usecase = CreateUser(repository);
@@ -21,6 +24,9 @@ void main() {
     () async {
       // Arrange
       // STUB
+
+      // in when we call the insider function with the help of the recieved
+      // dependency and expect an answer.
       when(
         () => repository.createUser(
           createdAt: any(named: 'createdAt'),
@@ -30,10 +36,19 @@ void main() {
       ).thenAnswer((_) async => const Right(null));
 
       //  Act
+      // here we call the function in the class inside which we are calling
+      // the method using the dependency.
       final result = await usecase(params);
 
       //  Assert
+      // and now we expect it returns the expected result.
       expect(result, equals(const Right<dynamic, void>(null)));
+
+      // and we verify the insider method which should have been
+      // called with the dependency is called properly and once.
+      // with the actual arguments this time.
+      // this time the arguments should be equal to the arguments
+      // we used in the act section above.
       verify(
         () => repository.createUser(
             createdAt: params.createdAt,
